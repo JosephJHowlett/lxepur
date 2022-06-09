4dea77fe79e3df2c1bdea6320ea1423757ee5c3f
Joseph Howlett <JosephJHowlett@gmail.com>
2022-06-09 15:44:15 -0400

    add st707

--- commissioning_lifetime_models.py
+++ commissioning_lifetime_models.py
@@ -135,3 +135,83 @@ if True:
     plt.savefig('plots/q5_trend.pdf')
     plt.savefig('plots/q5_trend.png', dpi=200)
 plt.show()
+
+
+
+### ST707
+
+if use_scada:
+    start_time = '01-19-21 00:00:00'
+    stop_time = '02-03-21 00:00:00'
+    df = sc.get_scada_values(
+        var_names,
+        start=string_to_lngs_epoch(start_time)*1e9,
+        end=string_to_lngs_epoch(stop_time)*1e9,
+        every_nth_value=100,
+        fill_gaps="forwardfill", # the DB stops recording values that do not change
+        query_type_lab=True,
+    )
+else:
+    df = pd.read_csv('data/st707commissioning_full.csv', index_col='time UTC', parse_dates=True)
+
+df['lifetime_err'] = get_errs(df.lifetime, df.anode_height, df.cathode_height, df.drift_time)
+df['lifetime_smooth'] = medfilt(df.lifetime, kernel_size=501)
+# clean
+df_clean = df[
+    (df.index < dt.datetime(2021, 1, 31, tzinfo=dt.timezone.utc))
+    & (df.lifetime > 0)
+]
+
+df_clean['tdiff_days'] = (df_clean.index - df_clean.index[0]).total_seconds()/60./60./24.
+
+
+def get_RHS(x, t, p):
+    rhs = p[1]/(1+(t/20))
+    rhs += -1.0*x/p[2]
+    rhs += -1.0*x/p[0]
+    return rhs
+
+
+outgassing = inv_outg(0.075e-3)  # g/d
+flow_speed = 60.0
+lxe_flow_speed = 2.0  # lpm
+efficiency = 0.57
+
+tmax = 12.0
+x0 = 0.00062
+
+
+times = np.linspace(0, tmax, 10000)
+
+tau = lxe_mass/density*1000/flow_speed*60/3600/24  # d
+tau2 = lxe_mass/lxe_density/lxe_flow_speed/efficiency*60/3600/24  # d
+print('outgassing', 'tau', 'tau2')
+print(outgassing, tau, tau2)
+p = [tau, outgassing / (lxe_mass/lxe_density), tau2]
+sol = odeint(
+    get_RHS,
+    x0,
+    times,
+    args=(p,)
+)
+p = [tau, outgassing / (lxe_mass/lxe_density), tau2]
+sol = odeint(
+    get_RHS,
+    x0,
+    times,
+    args=(p,)
+)
+fig = plt.figure(figsize=(12, 8))
+plt.plot(df_clean.tdiff_days, df_clean.lifetime_smooth, 'k.', rasterized=True)
+plt.fill_between(df_clean.tdiff_days, df_clean.lifetime_smooth-df_clean.lifetime_err, df_clean.lifetime_smooth+df_clean.lifetime_err, color='slategrey', alpha=0.3)
+plt.plot(times, 1.0/sol.T[0], 'C0--', linewidth=3)
+plt.xlim([0, 11])
+plt.ylim([1000, 8000])
+#plt.yscale('log')
+plt.grid(alpha=0.3)
+plt.xlabel('Time [d]')
+plt.ylabel('Electron Lifetime [$\mu$s]')
+if True:
+    plt.savefig('plots/st707_efficiency_trend_220526.pdf')
+    plt.savefig('plots/st707_efficiency_trend_220526.png', dpi=200)
+plt.show()
